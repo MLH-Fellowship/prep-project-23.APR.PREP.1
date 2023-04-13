8b1d18d36a4a24d919ec390599e50d59d99ccc14
Kristin Lam <klam623@gmail.com>
2023-04-12 22:50:09 -0400

    Style Day component, edit weather content

--- src/Day.js
+++ src/Day.js
@@ -1,5 +1,4 @@
 import './Day.css';
-
 import { padMinutes } from './helpers';
 
 const Day = ({ data }) => {
@@ -7,20 +6,42 @@ const Day = ({ data }) => {
   const dayDate = new Date(day.dt * 1000);
   const currDate = new Date(Date.now());
 
+  const dayWeather = day.weather[0];
+  const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][
+    dayDate.getDay()
+  ];
+
   return (
     <div className="day">
-      <h3>
-        {dayDate.getDate() === currDate.getDate()
-          ? 'Today'
-          : dayDate.getDate() === currDate.getDate() + 1
-          ? 'Tomorrow'
-          : dayDate.getDate()}
-      </h3>
-      <p>
-        {dayDate.getHours()}:{padMinutes(dayDate.getMinutes())}
-      </p>
-      <h4>{day.weather[0].main}</h4>
-      <p>Feels like {day.main.feels_like}°C</p>
+      <div className="day__icon">
+        <img
+          alt={dayWeather.description}
+          src={`https://openweathermap.org/img/wn/${dayWeather.icon}@2x.png`}
+        />
+      </div>
+
+      <div className="day__text">
+        <p className="day__date">
+          {dayDate.getDate() === currDate.getDate()
+            ? 'Today'
+            : `${dayOfWeek} ${dayDate.getDate()}`}
+        </p>
+
+        <p className="day__condition">{dayWeather.description}</p>
+
+        {/* <p className="day__text--time">
+          {dayDate.getHours()}:{padMinutes(dayDate.getMinutes())}
+        </p> */}
+
+        <p className="day__temp">
+          <span className="day__temp--max">
+            {Math.round(day.main.feels_like)}°
+          </span>
+          <span className="day__temp--min">
+            {Math.round(day.main.feels_like)}°
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
