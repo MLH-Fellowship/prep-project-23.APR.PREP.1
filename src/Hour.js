1bce038f35c6d954ded0c8ce8427166dbcef528f
Kristin Lam <klam623@gmail.com>
2023-04-13 18:53:55 -0400

    Begin styling Hour component

--- src/Hour.js
+++ src/Hour.js
@@ -1,27 +1,26 @@
-import './Hour.css'
-import { padLeftTwo } from './helpers'
+import './Hour.css';
+import { padLeftTwo } from './helpers';
 
 const Hour = ({ hour }) => {
   return (
-    <>
-      {/* hidden attribute should be set to false on click */}
-      <div className="hour">
-	<p className="hour__number">{padLeftTwo(hour.date.getHours())}</p>
-	<p className="hour__data">
-	  <div className="hour__icon">
-	    <img
-              alt={hour.weather[0].description}
-              src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}/>
-	  </div>
-	  <p className="hour__temp">
-	    <div className="hour__temp-real">
-	      {Math.round(hour.main.temp)}°
-	    </div>
-	  </p>
-	</p>
+    <div className="hour">
+      <div className="hour__number">
+        <p className="hour__number--hour">{`${padLeftTwo(
+          hour.date.getHours()
+        )}`}</p>
+        <p className="hour__number--min">00</p>
       </div>
-    </>
+
+      <div className="hour__icon">
+        <img
+          alt={hour.weather[0].description}
+          src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}
+        />
+      </div>
+
+      <p className="hour__temp">{Math.round(hour.main.temp)}°</p>
+    </div>
   );
-}
+};
 
 export default Hour;
