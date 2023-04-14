5b07ad562b5f7842ec903b503a0b2419a4f999f0
Kristin Lam <klam623@gmail.com>
2023-04-13 21:24:38 -0400

    Refactor Hour component into DayModal component

--- src/DayModal.js
+++ src/DayModal.js
@@ -1,11 +1,36 @@
-import Hour from './Hour';
+import './DayModal.css';
+import { padLeftTwo, dayOfWeek } from './helpers';
 
 const DayModal = ({ data }) => {
+  const day = data[0].date;
+
+  const renderedHours = data.map((hourInfo, idx) => (
+    <div key={idx} className="hour">
+      <div className="hour__number">
+        <p className="hour__number--hour">{`${padLeftTwo(
+          hourInfo.date.getHours()
+        )}`}</p>
+        <p className="hour__number--min">00</p>
+      </div>
+
+      <div className="hour__icon">
+        <img
+          alt={hourInfo.weather[0].description}
+          src={`https://openweathermap.org/img/wn/${hourInfo.weather[0].icon}.png`}
+        />
+      </div>
+
+      <p className="hour__temp">{Math.round(hourInfo.main.temp)}°</p>
+    </div>
+  ));
+
   return (
-    <div>
-      {data.map((hourInfo, idx) => (
-        <Hour key={idx} hour={hourInfo} />
-      ))}
+    <div className="modal">
+      <p className="modal__heading">
+        {dayOfWeek(day)} {day.getDate()}
+      </p>
+
+      <div className="modal__hours">{renderedHours}</div>
     </div>
   );
 };
