8bf52a4a017854d2ef05d15c74f6af059d0f858a
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2023-04-15 19:15:28 -0400

    added mist "

--- src/essentials.js
+++ src/essentials.js
@@ -1,12 +1,12 @@
 function Essentials (props){
-    if (props.today === "Clear")  {
+    if (props.today === "Sunny")  {
           return (
             <p>Make sure to bring sunscreen, sunglasses, a hat, and a water bottle for the sunny weather. Also, wear light and breathable clothing.</p>)
                         }
-    else if (props.today === "Clouds") {
+    else if (props.today === "Cloudy") {
         return (
           // <h1>Essentials</h1>
-          <p>bring a light jacket or sweater for potential temperature changes. also get sunglasses in case the clouds break and the sun comes out</p>)
+          <p>Bring a light jacket or sweater for potential temperature changes. also get sunglasses in case the clouds break and the sun comes out</p>)
                       }
     else if (props.today === "Rain") {
         return (
@@ -23,7 +23,7 @@ function Essentials (props){
           // <h1>Essentials</h1>
           <p>Stay indoors and avoid going outside until the storm passes. </p>)
                       }
-    else if (props.today === "Drizzle") {
+    else if (props.today === "Drizzle" || props.today === "Mist") {
         return (
           // <h1>Essentials</h1>
           <p>Consider bringing a waterproof jacket or a raincoat to stay dry. It's also a good idea to have an umbrella</p>)
