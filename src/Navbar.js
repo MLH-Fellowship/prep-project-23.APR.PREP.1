ec0ad0187a767a0ab14a328e88b44e001bb485f0
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-04-18 00:36:26 +0530

    added navbar (#10)
    * added navbar
    
    * added package.json
    
    * add shadow to differentiate navbar from body
    
    ---------
    
    Co-authored-by: Aksh Gupta <akshgpt7@gmail.com>

---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import App from "./App";
+import "./Navbar.css"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/trip-planner">Trip Planner</Link>
+            </li>
+            <li>
+              <Link to="/climate-change">Climate Change</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About us</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+        <Route exact path="/" component={App} />
+        <Route path="/climate-change" component={App} />
+        <Route path="/trip-planner" component={App} />
+        <Route path="/about-us" component={App} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default Navbar;
