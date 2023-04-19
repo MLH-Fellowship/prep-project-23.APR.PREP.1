d043c8b79c2edcc788b44620f9f7bc460395b506
Ankur Agarwal <74523865+Ankur6702@users.noreply.github.com>
2023-04-19 23:12:11 +0530

    Made application mobile friendly (#22)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AutoCity from "./components/AutoCity";
 import Forecast from './Forecast';
 import React  from 'react';
 
-
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -41,30 +40,25 @@ function App() {
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
-    return (
-      <>
-        <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-        <div>
-	  <h2>Enter a city below <span role="img" aria-label="emoji">👇</span></h2>
-	  <AutoCity onSelect={handleSelect} />
-	  <div className="Results">
-	    {!isLoaded && <h2>Loading...</h2>}
-	    {isLoaded && results && (
-	      <>
-                <h3>{results.weather[0].main}</h3>
-                <p>Feels like {results.main.feels_like}°C</p>
-                <i>
-		  <p>
-		    {results.name}, {results.sys.country}
-		  </p>
-                </i>
-		<Forecast city={city} />
-	      </>
-	    )}
-	  </div>
+    return <>
+      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+      <div className="container">
+        <div className="header">
+          <h2>Enter a city below <span role="img" aria-label="emoji">👇</span></h2>
+          <AutoCity onSelect={handleSelect} />
         </div>
-      </>
-    );
+        <div className="results">
+          {!isLoaded && <h2>Loading...</h2>}
+          {console.log(results)}
+          {isLoaded && results && (<>
+            <h3>{results.weather[0].main}</h3>
+            <p>Feels like {results.main.feels_like}°C</p>
+            <i><p>{results.name}, {results.sys.country}</p></i>
+            <Forecast city={city} />
+          </>)}
+        </div>
+      </div>
+    </>
   }
 }
 
