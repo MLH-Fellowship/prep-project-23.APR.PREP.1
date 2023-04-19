82c0ad662bbc9b62cebc8d6d547fff0f19503daa
Kristin Lam <klam623@gmail.com>
2023-04-18 21:23:48 -0400

    Update component with bar chart and air quality index

--- src/components/climate/AirQuality.js
+++ src/components/climate/AirQuality.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  Line,
+} from 'recharts';
 
 const AirQuality = ({ coordinates }) => {
-  console.log(coordinates);
   const { lon, lat } = coordinates;
-
   const [results, setResults] = useState(null);
+  const [airQualityIndex, setAirQualityIndex] = useState(null);
+
+  const airQualityMap = {
+    1: 'Good',
+    2: 'Fair',
+    3: 'Moderate',
+    4: 'Poor',
+    5: 'Very Poor',
+  };
 
   useEffect(() => {
     if (lon && lat) {
@@ -14,12 +31,16 @@ const AirQuality = ({ coordinates }) => {
         .then((res) => res.json())
         .then(
           (result) => {
+            const index = result.list[0].main.aqi;
+            setAirQualityIndex(airQualityMap[index]);
             const airQualityObj = result.list[0].components;
             const airQualityArray = Object.entries(airQualityObj).map(
-              ([name, value]) => ({
-                name,
-                value,
-              })
+              ([name, value]) => {
+                return {
+                  name,
+                  value,
+                };
+              }
             );
             setResults(airQualityArray);
           },
@@ -30,7 +51,27 @@ const AirQuality = ({ coordinates }) => {
     }
   }, [coordinates]);
 
-  return <div>Work in progress</div>;
+  return (
+    <div>
+      <div>
+        <h2>Air Quality</h2>
+      </div>
+
+      {results && (
+        <div>
+          <p>Air Quality Index: {airQualityIndex}</p>
+          <BarChart width={730} height={300} data={results}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" fill="#8884d8" />
+          </BarChart>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AirQuality;
