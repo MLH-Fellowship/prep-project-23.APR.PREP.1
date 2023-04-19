14ca70602f81fa0237645aeedbca0d4eacd700a0
quanrong <quanrong@mailbox.org>
2023-04-19 11:19:02 +0200

    Add basic proxy function for current weather API

---
+++ src/functions/weather/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleError(code int, err error) (*events.APIGatewayProxyResponse, error) {
+	log.Println(err.Error())
+	return &events.APIGatewayProxyResponse{
+		StatusCode: code,
+		Body:       err.Error(),
+	}, err
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	uri := fmt.Sprintf(
+		"https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s",
+		request.QueryStringParameters["q"],
+		os.Getenv("REACT_APP_APIKEY"))
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return handleError(502, err)
+	}
+
+	strResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return handleError(500, err)
+	}
+
+	var parsResp map[string]any
+	json.Unmarshal([]byte(strResp), &parsResp)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: resp.StatusCode,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(strResp),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
