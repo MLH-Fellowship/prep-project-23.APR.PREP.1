5a20dffd808747123976696960087b2b56bc9c1d
Kristin Lam <klam623@gmail.com>
2023-04-21 20:52:45 -0400

    Fix duplicate useEffect error that broke weather API calls

--- src/App.js
+++ src/App.js
@@ -1,90 +1,73 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import GMaps from './Map' 
-import logo from "./mlh-prep.png";
-import WeatherOverlay from "./components/WeatherOverlay";
-import AutoCity from "./components/AutoCity";
+import GMaps from './Map';
+import logo from './mlh-prep.png';
+import WeatherOverlay from './components/WeatherOverlay';
+import AutoCity from './components/AutoCity';
 import Forecast from './Forecast';
-import React  from 'react';
-import Essentials from "./components/essentials";
+import Essentials from './components/essentials';
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState('');
   const [results, setResults] = useState(null);
-  const [cood, setCood] = useState({lat: 40.7127753, lng: -74.0059728})
-  console.log(cood, "COORDINATE STATE")
+  const [cood, setCood] = useState({ lat: 40.7127753, lng: -74.0059728 });
+  const [containerStyle, setContainerStyle] = useState({
+    backgroundImage: `url(/assets/weather-icons/Clouds.svg)`,
+  });
 
   function getCityLocation(city) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.REACT_APP_GMAPS}`)
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.REACT_APP_GMAPS}`
+    )
       .then((response) => {
-          return response.json();
-      }).then(jsonData => {
-        setCood(jsonData.results[0].geometry.location); 
+        return response.json();
       })
+      .then((jsonData) => {
+        setCood(jsonData.results[0].geometry.location);
+      });
   }
 
-
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=metric' +
+        '&appid=' +
+        process.env.REACT_APP_APIKEY
+    )
+      .then((res) => res.json())
       .then(
         (result) => {
           if (result['cod'] !== 200) {
-            setIsLoaded(false)
+            setIsLoaded(false);
           } else {
             setIsLoaded(true);
             setResults(result);
+            setContainerStyle(getContainerStyle(results.weather[0].main));
           }
         },
         (error) => {
           setIsLoaded(true);
           setError(error);
         }
-      )
-  }, [city])
+      );
+  }, [city]);
   //  const basename = process.env.REACT_APP_URL;
   //  const uri = basename + '/api/proxy?api=weather&q=' + city +
   //              '&units=metric';
 
-  const uri = 'https://api.openweathermap.org/data/2.5/weather?&q=' + city +
-  	      '&units=metric&appid=' + process.env.REACT_APP_APIKEY;
+  const uri =
+    'https://api.openweathermap.org/data/2.5/weather?&q=' +
+    city +
+    '&units=metric&appid=' +
+    process.env.REACT_APP_APIKEY;
 
   const handleSelect = (suggestion) => {
     setCity(suggestion.name);
-    getCityLocation(suggestion.name)
+    getCityLocation(suggestion.name);
   };
-  const [containerStyle, setContainerStyle] = useState({
-    backgroundImage: `url(/assets/weather-icons/Clouds.svg)`
-  });
-
-  useEffect(() => {
-    if (city) {
-      fetch(uri)
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            if (result["cod"] === 200) {
-              setIsLoaded(false);
-            } else {
-              setIsLoaded(true);
-              setResults(result);
-            }
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        );
-    }
-  }, [city, uri]);
-
-  useEffect(() => {
-    if (isLoaded && results) {
-      setContainerStyle(getContainerStyle(results.weather[0].main));
-    }
-  }, [isLoaded, results]);
 
   function getContainerStyle(weather) {
     return { backgroundImage: `url(/assets/weather-icons/${weather}.svg)` };
@@ -98,24 +81,31 @@ function App() {
         <img className="logo" src={logo} alt="MLH Prep Logo"></img>
         <div className="container">
           <div className="header">
-            <h2>Enter a city below <span role="img" aria-label="emoji">👇</span></h2>
+            <h2>
+              Enter a city below{' '}
+              <span role="img" aria-label="emoji">
+                👇
+              </span>
+            </h2>
             <AutoCity onSelect={handleSelect} />
           </div>
-          <GMaps
-            cood={cood}
-            setCood={setCood}
-          />
+          <GMaps cood={cood} setCood={setCood} />
           <WeatherOverlay style={containerStyle} />
           <div className="results">
             {!isLoaded && <h2>Loading...</h2>}
             {console.log(results)}
             {isLoaded && results && (
-            <>
-              <h3>{results.weather[0].main}</h3>
-              <p>Feels like {results.main.feels_like}°C</p>
-              <i><p>{results.name}, {results.sys.country}</p></i>
-              <Forecast city={city} />
-            </>)}
+              <>
+                <h3>{results.weather[0].main}</h3>
+                <p>Feels like {results.main.feels_like}°C</p>
+                <i>
+                  <p>
+                    {results.name}, {results.sys.country}
+                  </p>
+                </i>
+                <Forecast city={city} />
+              </>
+            )}
           </div>
         </div>
       </>
