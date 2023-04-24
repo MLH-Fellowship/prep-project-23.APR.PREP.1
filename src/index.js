5fa607f726d88073700373659f311a94c94fb476
Abirami Karthikeyan <97060120+k-abirami@users.noreply.github.com>
2023-04-24 14:21:54 -0400

    Graphs and cards for climate change page  (#52)
    * Create component that fetches love air quality data with coordinates
    
    * Create Climate Page, pass down coordinates to AirQuality component
    
    * Update component with bar chart and air quality index
    
    * Update charts package
    
    * Update navbar with climate change page
    
    * Update name of y-axis value on chart
    
    * Add error handling to ClimatePage
    
    * Added the climate graph and cards for climate change tips
    
    * Added the climate graph and cards for climate change tips
    
    * Remove comments and line breaks
    
    * Add climate page to navbar
    
    * Remove extra letter from README
    
    * Add CSS reset styles and fotn family
    
    * Style and restructure climate change page
    
    * Update state for readability
    
    * Fix error handling for climate page
    
    * Fix error handling on climate page
    
    * Fix error handling for Air Quality chart
    
    * Fix error message
    
    * Fix duplicate useEffect error that broke weather API calls
    
    * Refactor functions for consistency, remove unused variables
    
    ---------
    
    Co-authored-by: Kristin Lam <klam623@gmail.com>
    Co-authored-by: Kristin Lam <10857587+kristinlam@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/Navbar';
 import App from './App';
